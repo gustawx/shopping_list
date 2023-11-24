fb3a1c9be2e1c1238be267ce71c0d873f7e34e27
gustawx <haslO1234?>
2023-11-24 11:49:02 +0100

    feat(grocery_list.dart, new_item.dart): add Firebase integration for grocery list - Load grocery items from Firebase in grocery_list.dart - Save new items to Firebase in new_item.dart
    chore(pubspec.yaml): add http package to dependencies for HTTP requests to Firebase

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,15 +15,48 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
-  void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
-      MaterialPageRoute(builder: (ctx) => const NewItem()),
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  List<GroceryItem> _groceryItems = [];
+  void _loadItems() async {
+    final url = Uri.https(
+      "flutter-course-e8679-default-rtdb.europe-west1.firebasedatabase.app",
+      "shopping-list.json",
     );
-    if (newItem == null) return;
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      _loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
     setState(() {
-      _groceryItems.add(newItem);
+      _groceryItems = _loadedItems;
     });
+  }
+
+  void _addItem() async {
+    await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+    _loadItems();
   }
 
   @override
